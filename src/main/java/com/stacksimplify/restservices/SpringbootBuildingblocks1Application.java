c7ac68a4ba12997563bed65a1dfae04a6b57029a
rajanishad <prafullarajanishad3908@gmail.com>
2022-03-14 18:34:34 +0530

    First Commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocks1Application.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocks1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocks1Application.class, args);
+	}
+
+}
