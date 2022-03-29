f998ea3db99f05afcbd7c79fc4864b34ff8cd48e
rajanishad <prafullarajanishad3908@gmail.com>
2022-03-29 20:52:47 +0530

    Map struct first commit

---
+++ src/main/java/com/stacksimplify/restservices/mappers/UserMapper.java
@@ -0,0 +1,30 @@
+package com.stacksimplify.restservices.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+import com.stacksimplify.restservices.dtos.UserMsDto;
+import com.stacksimplify.restservices.entities.User;
+
+@Mapper(componentModel = "Spring")
+public interface UserMapper {
+
+	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+	
+	//user to user dto
+	@Mappings({
+		@Mapping(source = "email",target = "emailaddress"),
+		@Mapping(source = "role",target = "rolename")
+		})
+	
+	
+	UserMsDto userToUserDto(User user);
+	
+	//list<User> to list<UserMsDto>
+	List<UserMsDto> usersToUserDtos(List<User> users);
+	
+}
